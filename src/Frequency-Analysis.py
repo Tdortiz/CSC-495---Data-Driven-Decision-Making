cd0c30812ecfca5fed825ba105ec0da810e1771b
Thomas Ortiz <tdortiz@ncsu.edu>
2017-09-05 01:49:20 -0400

    Restructured folder structure

--- src/Frequency-Analysis.py
+++ src/Frequency-Analysis.py
@@ -7,10 +7,13 @@ import sys
 import io
 import nltk
 import string
+from pathlib import Path
 from nltk.probability import FreqDist
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 from textblob import TextBlob
+
+repo_dir = Path(__file__).resolve().parents[1]
 
 stop_words = list(stopwords.words('english')) + list(string.punctuation)
 stop_words_economic = ['it', '\'s', '--', '\'\'', 'For', 'As', 'physicians', 'Alex', 'NNY']
@@ -24,10 +27,7 @@ stop_words_political = []
 stop_words.extend(stop_words_economic + stop_words_technical + stop_words_legal + stop_words_ethical + stop_words_procedural + stop_words_political)
 
 # open the file
-script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
-rel_path = "Hospitals-Economic/xx01"
-abs_path = os.path.join(script_dir, rel_path)
-data = open(abs_path, encoding='utf-8').read()
+data = open(os.path.join(repo_dir, "Hospitals-Economic/xx01"), encoding='utf-8').read()
 
 # print(data) # just a test
 words = word_tokenize(data)
@@ -38,25 +38,8 @@ for w in words:
 
 # Plot the graph
 spread = nltk.FreqDist(words_selected)
-#spread.plot(50, cumulative=True)
+spread.plot(50, cumulative=True)
 
 # Print the word - frequency
-freq_words = []
-print("\tWORD - FREQUENCY")
 for word, frequency in spread.most_common(100):
     print(u'{} - {}'.format(word, frequency))
-    freq_words.append(word)
-
-bunch = TextBlob(data)
-all_sentences = bunch.sentences
-key_words = ['tax', 'taxes', 'economic', 'economy', 'payroll', 'finance', 'price', 'cost', 'fine', 'cutback', 'bill']
-freq_words.extend(key_words)
-
-output = open(os.path.join(script_dir, 'textblob-freq.txt'), 'w')
-sent_num = 0
-for sentence in all_sentences:
-    if(any(map(lambda word: word in sentence, freq_words))):
-        print(str(sent_num) + ":\t" + str(sentence) + "\n")
-        sent_num += 1
-        output.write(str(sent_num) + ":\t" + str(sentence) + "\n\n")
-output.close()
--- src/TextBlob.py
+++ src/TextBlob.py
@@ -3,20 +3,19 @@
 """
 
 import os
+from pathlib import Path
 from textblob import TextBlob
 
 # open the file
-script_dir = os.path.dirname(__file__) # absolute dir the script is in
-rel_path = "Hospitals-Economic/xx01"
-abs_path = os.path.join(script_dir, rel_path)
-data = open(abs_path, encoding='utf-8').read()
+repo_dir = Path(__file__).resolve().parents[1]
+data = open(os.path.join(repo_dir, "Hospitals-Economic/xx01"), encoding='utf-8').read()
 bunch = TextBlob(data)
 all_sentences = bunch.sentences
 
 # Create key words
 key_words = ['tax', 'taxes', 'economic', 'economy', 'payroll', 'finance', 'price', 'cost', 'fine', 'cutback', 'money', 'wage', 'salary']
 
-output = open(os.path.join(script_dir, 'textblob.txt'), 'w')
+output = open(os.path.join(repo_dir, 'output/textblob.txt'), 'w')
 sent_num = 0
 for sentence in all_sentences:
     if(any(map(lambda word: word in sentence, key_words))):
