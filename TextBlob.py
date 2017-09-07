740bc5d9bced6ee50b521993e05202a915304557
Parag Nakhwa <psnakhwa@ncsu.edu>
2017-09-07 02:44:30 -0400

    added keywords for legal

---
+++ TextBlob.py
@@ -0,0 +1,26 @@
+"""
+@author: tdortiz
+"""
+
+import os
+from textblob import TextBlob
+
+# open the file
+script_dir = os.path.dirname(__file__) # absolute dir the script is in
+rel_path = "Hospitals-Economic/xx01"
+abs_path = os.path.join(script_dir, rel_path)
+data = open(abs_path, encoding='utf-8').read()
+bunch = TextBlob(data)
+all_sentences = bunch.sentences
+
+# Create key words
+key_words = ['tax', 'taxes', 'economic', 'economy', 'payroll', 'finance', 'price', 'cost', 'fine', 'cutback', 'money', 'wage', 'salary']
+key_words_legal = ['hospitals','patients','health','legal','documents','medical','court','children','reserved','publication','newspaper','doctors','rights','government','news','language','copyright','services']
+output = open(os.path.join(script_dir, 'textblob.txt'), 'w')
+sent_num = 0
+for sentence in all_sentences:
+    if(any(map(lambda word: word in sentence, key_words))):
+        print(str(sent_num) + ":\t" + str(sentence) + "\n")
+        sent_num += 1
+        output.write(str(sent_num) + ":\t" + str(sentence) + "\n\n")
+output.close()
