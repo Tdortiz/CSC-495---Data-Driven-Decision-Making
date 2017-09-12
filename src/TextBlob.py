1b3e45ea564476e79f08603c63d787f7d95b8d10
Thomas Ortiz <tdortiz@ncsu.edu>
2017-09-12 14:16:49 -0400

    Used "With" to open files

--- src/TextBlob.py
+++ src/TextBlob.py
@@ -8,9 +8,9 @@ from textblob import TextBlob
 
 # open the file
 repo_dir = Path(__file__).resolve().parents[1]
-data = open(os.path.join(repo_dir, "input/Hospital-Ranking/hospital-ranking.TXT"), encoding='utf-8').read()
-bunch = TextBlob(data)
-all_sentences = bunch.sentences
+with open(os.path.join(repo_dir, "input/Hospital-Ranking/hospital-ranking.TXT"), encoding='utf-8')as data:
+    bunch = TextBlob(data.read())
+    all_sentences = bunch.sentences
 
 # Create key words
 key_words            = set()
@@ -30,11 +30,10 @@ key_words_political  = set()
 # add all key words together
 key_words |= key_words_economic | key_words_technical | key_words_legal | key_words_ethical | key_words_procedural | key_words_political
 
-output = open(os.path.join(repo_dir, 'output/' + os.path.splitext(__file__)[0] + '_output.txt'), 'w', encoding='utf-8')
-sent_num = 0
-for sentence in all_sentences:
-    if(any(map(lambda word: word in sentence, key_words))):
-        print(str(sent_num) + ":\t" + str(sentence) + "\n")
-        sent_num += 1
-        output.write(str(sent_num) + ":\t" + str(sentence) + "\n\n")
-output.close()
+with open(os.path.join(repo_dir, 'output/' + os.path.splitext(__file__)[0] + '_output.txt'), 'w', encoding='utf-8') as output:
+    sent_num = 0
+    for sentence in all_sentences:
+        if(any(map(lambda word: word in sentence, key_words))):
+            print(str(sent_num) + ":\t" + str(sentence) + "\n")
+            sent_num += 1
+            output.write(str(sent_num) + ":\t" + str(sentence) + "\n\n")
