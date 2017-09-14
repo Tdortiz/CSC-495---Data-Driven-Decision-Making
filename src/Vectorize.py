4fc3b1650ff0c6f211926ea8ceb5e150081accc3
Thomas Ortiz <tdortiz@ncsu.edu>
2017-09-14 14:31:15 -0400

    Final Vectorize file

--- src/Vectorize.py
+++ src/Vectorize.py
@@ -1,10 +1,21 @@
 import os
+import operator
 from pathlib import Path
 from sklearn.feature_extraction.text import CountVectorizer
 import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
+
+def dump_keys(d, lvl=0):
+    for k, v in d.items():
+        spacing = lvl * ' '
+        key = '{:15}'.format(str(k))
+        value = ' - ' + str(v)
+        # print('%s%s' % (lvl * ' ', k) + " -- " + str(v))
+        print( spacing + key + value)
+        if type(v) == dict:
+            dump_keys(v, lvl+1)
 
 tf = TfidfVectorizer(analyzer='word', ngram_range=(1,3), min_df = 0, stop_words = 'english')
 
@@ -16,5 +27,10 @@ with open(os.path.join(repo_dir, "input/Hospital-Ranking/hospital-ranking.TXT"),
 vectorizer = TfidfVectorizer(min_df=1)
 X = vectorizer.fit_transform(words)
 idf = vectorizer.idf_
-print (dict(zip(vectorizer.get_feature_names(), idf)))
-#print(intake)
+
+# Get dictionary
+my_dict = dict(zip(vectorizer.get_feature_names(), idf))
+# Sort it from most value to least
+my_dict = dict(sorted(my_dict.items(), key=operator.itemgetter(1)))
+# Print the dictionary
+dump_keys(my_dict)
