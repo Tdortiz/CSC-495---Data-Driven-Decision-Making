eb3b53cbf88dd9c40f4d094b6d39b688fd181b82
raayers <31638957+raayers@users.noreply.github.com>
2017-09-14 13:36:11 -0400

    Added words for technical

--- src/Frequency-Analysis.py
+++ src/Frequency-Analysis.py
@@ -18,7 +18,7 @@ repo_dir = Path(__file__).resolve().parents[1]
 # Create stop words
 stop_words            = set(stopwords.words('english')) | set(string.punctuation)
 stop_words_economic   = {'it', '\'s', '--', '\'\'', 'For', 'As', 'physicians', 'Alex', 'NNY'}
-stop_words_technical  = set()
+stop_words_technical  = {'it','this','including','rights','reserved','US','2015','all','years','also','we','was','not','care','as','he','documents','more',''','have','from','their','has','will','be','said',''s','at','(',')','are','by','on','``','','"','with','as','that','is','for','all','in','to','of','and','the','.',':','*','`','2016','been','one','who','our','its','about','i','or','can','but',';','-','`','a','an','such','http','were','there','other','major','which','they','1000','copyright','words','new','may','well','his','next','into','$','&','--','some','had','up','than','key','would','do','over'}
 stop_words_legal      = {'it', '\'s', '--', '\'\'', 'For', 'As', 'the', 'The','All',',','\'','\"','hospital','Hospital','legal','said','also','patient','would','But','could'}
 stop_words_ethical    = set()
 stop_words_procedural = {'Royal', 'two', 'TEE', 'year', 'B.C', 'St.', 'even' }
