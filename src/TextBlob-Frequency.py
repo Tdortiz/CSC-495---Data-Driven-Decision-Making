e189017274e22a5071ffce1bae0269bda11e7ee5
Parag Nakhwa <psnakhwa@ncsu.edu>
2017-09-25 23:28:53 -0400

    added keywords for ethical and technical

--- src/TextBlob-Frequency.py
+++ src/TextBlob-Frequency.py
@@ -18,11 +18,11 @@ repo_dir = Path(__file__).resolve().parents[1]
 # Create stop words
 stop_words            = set(stopwords.words('english')) | set(string.punctuation)
 stop_words_economic   = {'it', '\'s', '--', '\'\'', 'For', 'As', 'physicians', 'Alex', 'NNY'}
-stop_words_technical  = set()
-stop_words_legal      = set()
-stop_words_ethical    = set()
-stop_words_procedural = set()
-stop_words_political  = set()
+stop_words_technical  = {'it','technical','Technical','per','12','this','including','rights','reserved','US','2015','all','years','also','we','was','not','care','as','he','documents','more','','have','from','their','has','will','be','said','s','at','(',')','are','by','on','``','','"','with','as','that','is','for','all','in','to','of','and','the','.',':','*','`','2016','been','one','who','our','its','about','i','or','can','but',';','-','`','a','an','such','http','were','there','other','major','which','they','1000','copyright','words','new','may','well','his','next','into','$','&','--','some','had','up','than','key','would','do','over'}
+stop_words_legal      = {'it', '\'s', '--', '\'\'', 'For', 'As', 'the', 'The','All',',','\'','\"','hospital','Hospital','legal','said','also','patient','would','But','could'}
+stop_words_ethical    = {'In','It','If','Pg','1','n\'t','See','A','Ethics','patients','997','must','says','Dr.','doctors','ethical','Ethical','ethics','medical','health','make','need','Health'}
+stop_words_procedural = {'Royal', 'two', 'TEE', 'year', 'B.C', 'St.', 'even' }
+stop_words_political  = {'This', 'hospitals', 'He', 'state', 'years', 'last', 'No', 'Dr', 'staff', 'one', 'We', 'I', 'local'}
     
 # add all stop words together
 stop_words |= stop_words_economic | stop_words_technical | stop_words_legal | stop_words_ethical | stop_words_procedural | stop_words_political
