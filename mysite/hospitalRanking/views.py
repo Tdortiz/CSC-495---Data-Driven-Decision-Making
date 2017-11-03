a10ac61c2b353d17b66e5493472f7dea4f71fdba
Thomas Ortiz <tdortiz@ncsu.edu>
2017-11-02 23:10:34 -0400

    Sample form

--- mysite/hospitalRanking/views.py
+++ mysite/hospitalRanking/views.py
@@ -6,19 +6,22 @@ from django.http import Http404, JsonResponse
 # Create your views here.
 def submit_ranking_form(request):
     if request.method == 'POST':
+
         # Example of how to get the form values
-        pe_1 = request.POST.getlist('priorityExample1')[0]
-        pe_2 = request.POST.getlist('priorityExample2')[0]
-        pe_3 = request.POST.getlist('priorityExample3')[0]
+        zipcode = request.POST['zipcode']
+        pe_1 = request.POST['priorityExample1']
+        pe_2 = request.POST['priorityExample2']
+        pe_3 = request.POST['priorityExample3']
 
         context = {
+            'zipcode': str(zipcode),
             'pe1': str(pe_1),
             'pe2': str(pe_2),
             'pe3': str(pe_3),
         }
-
         print(str(context))
-        return render(request, 'hospitalRanking/results.html', context)
+
+        return render(request, 'hospitalRanking/results.html', {'context': context})
     else:
         raise Http404("Invalid request")
 
