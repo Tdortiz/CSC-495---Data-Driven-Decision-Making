d7e863b28d325cd00611805e020528ea9d0476e8
Thomas Ortiz <tdortiz@ncsu.edu>
2017-11-03 00:55:12 -0400

    Making form prettier

--- mysite/hospitalRanking/views.py
+++ mysite/hospitalRanking/views.py
@@ -8,16 +8,20 @@ def submit_ranking_form(request):
     if request.method == 'POST':
 
         # Example of how to get the form values
-        zipcode = request.POST['zipcode']
+        location = request.POST['location']
+        distance_in_miles = request.POST['distanceInMiles']
         pe_1 = request.POST['priorityExample1']
         pe_2 = request.POST['priorityExample2']
         pe_3 = request.POST['priorityExample3']
+        pe_4 = request.POST['priorityExample4']
 
         context = {
-            'zipcode': str(zipcode),
+            'location': str(location),
+            'distanceInMiles': str(distance_in_miles),
             'pe1': str(pe_1),
             'pe2': str(pe_2),
             'pe3': str(pe_3),
+            'pe4': str(pe_4),
         }
         print(str(context))
 
