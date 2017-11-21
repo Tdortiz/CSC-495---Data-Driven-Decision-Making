1757de50d15889a24db5d723247cce9f26471a10
Thomas Ortiz <tdortiz@ncsu.edu>
2017-11-21 14:08:53 -0500

    Updating post names

--- mysite/hospitalRanking/views.py
+++ mysite/hospitalRanking/views.py
@@ -13,19 +13,21 @@ def submit_ranking_form(request):
         # Example of how to get the form values
         location = request.POST['location']
         distance_in_miles = request.POST['distanceInMiles']
-        pe_1 = request.POST['priorityExample1']
-        pe_2 = request.POST['priorityExample2']
-        pe_3 = request.POST['priorityExample3']
-        pe_4 = request.POST['priorityExample4']
+        cost_of_care = request.POST['CostOfCare']
+        timely_effective_care = request.POST['TimelyEffectiveCare']
+        complications_and_deaths = request.POST['ComplicationsAndDeaths']
+        hospital_returns = request.POST['HospitalReturns']
+        doctor_ranking = request.POST['DoctorRank']
 
         context = {
             'filters': {
                 'location': str(location),
                 'distanceInMiles': str(distance_in_miles),
-                'pe1': str(pe_1),
-                'pe2': str(pe_2),
-                'pe3': str(pe_3),
-                'pe4': str(pe_4),
+                'cost_of_care': str(cost_of_care),
+                'timely_effective_care': str(timely_effective_care),
+                'complications_and_deaths': str(complications_and_deaths),
+                'hospital_returns': str(hospital_returns),
+                'doctor_ranking': str(doctor_ranking)
             },
             'hospitals': []
         }
@@ -44,16 +46,11 @@ def submit_ranking_form(request):
 
 
 def get_hospital(request, hospital_id):
-    print("\tHospital ID = [" + str(hospital_id) + "]")
-
     hospitals = Hospital.objects.filter(provider_id=hospital_id)
     if hospitals.exists():
         context = {
             'hospital': hospitals[0]
         }
-
-        print("\tHospital = " + str(hospitals[0]))
-
         return render(request, 'hospitalRanking/hospital.html', context)
     else:
         raise Http404("Invalid request")
